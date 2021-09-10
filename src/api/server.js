7e7e633dd36f5a199c3d90a6dfbeac2c2ef23af7
Borys Olejniczak <borys.olejniczak@antarisconsulting.com>
2021-09-10 15:57:21 +0100

    Fix server

--- src/api/server.js
+++ src/api/server.js
@@ -1,20 +1,21 @@
-const { urlencoded } = require('express');
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 4000;
-const gutars = require('./guitars');
+const guitars = require("./guitars");
 
-app.use(express / urlencoded({ extended: true }));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-app.get('/guitars', (req, res) => {
-  res.json(gutars);
+app.get("/guitars", (req, res) => {
+  res.json(guitars);
 });
 
 app.listen(PORT, () => {
